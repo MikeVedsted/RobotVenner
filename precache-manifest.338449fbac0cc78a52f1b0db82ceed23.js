6407c814141394013786d1c666962e7df2774cdd
Mike Vedsted <mikevedsted@gmail.com>
2020-01-07 18:35:12 +0200

    Updates

---
+++ precache-manifest.338449fbac0cc78a52f1b0db82ceed23.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b1b61d0e74f25728295312daf1e9d94",
+    "url": "/RobotVenner/index.html"
+  },
+  {
+    "revision": "3b73c8d24a45c7e2626e",
+    "url": "/RobotVenner/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b2ce4dd316f7431632a4",
+    "url": "/RobotVenner/static/css/main.5d3d21c4.chunk.css"
+  },
+  {
+    "revision": "3b73c8d24a45c7e2626e",
+    "url": "/RobotVenner/static/js/2.c13d78d9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/RobotVenner/static/js/2.c13d78d9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b2ce4dd316f7431632a4",
+    "url": "/RobotVenner/static/js/main.4c45812b.chunk.js"
+  },
+  {
+    "revision": "686f63c5c967036c5831",
+    "url": "/RobotVenner/static/js/runtime-main.7ed60ecb.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RobotVenner/static/media/SEGA.12a3db8b.woff"
+  }
+]);
